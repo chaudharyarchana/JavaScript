e24625cff569b2c589ae5aae51a03c8e6f4f2868
Archana Kumari <archanakumari123.co@gmail.com>
2024-02-08 17:50:33 +0530

    "swap number without temp"

--- primitive/Question1.js
+++ primitive/Question1.js
@@ -3,9 +3,19 @@
 let num1 =6;
 let num2 =9;
 
+//method 1
 num1 =num1+num2;
 num2 =num1-num2;
 num1 = num1 - num2;
 
 console.log(num1,num2)
+
+//method 2
+
+let x= 2;
+let y=7;
+
+[x,y] = [y,x]
+
+console.log(x,y)
 
