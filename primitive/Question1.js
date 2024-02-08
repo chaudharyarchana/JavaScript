7962e4ae860e5b4f3e0ed1204c739a3f524b0194
Archana Kumari <archanakumari123.co@gmail.com>
2024-02-08 17:48:25 +0530

    "swap number without temp"

---
+++ primitive/Question1.js
@@ -0,0 +1,11 @@
+// Swap two integers present in variables num1 and num2 without using temporary variable.
+
+let num1 =6;
+let num2 =9;
+
+num1 =num1+num2;
+num2 =num1-num2;
+num1 = num1 - num2;
+
+console.log(num1,num2)
+
