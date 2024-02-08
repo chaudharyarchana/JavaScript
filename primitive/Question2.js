69d88c28dfaefff1c9b08653d2b997f2d5fe844e
Archana Kumari <archanakumari123.co@gmail.com>
2024-02-08 17:57:33 +0530

    "check integer without inbuild"

---
+++ primitive/Question2.js
@@ -0,0 +1,8 @@
+// Write a function which returns true if given value of number is an integer without using any inbuilt functions
+
+function checkInteger(num){
+    return num % 1 === 0
+}
+
+console.log(checkInteger(1))
+
