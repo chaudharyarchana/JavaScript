b076beaef93cbe5880a5f5aa972dd17e96fd7475
Archana Kumari <archanakumari123.co@gmail.com>
2024-02-09 18:07:58 +0530

    "based on slice"

---
+++ primitive/Question10.js
@@ -0,0 +1,11 @@
+// Write a function to truncate a string to a certain number of letters.
+// truncateString("JavaScript", 7); // "Java..." 
+
+//Method 1 - using slice
+//Method 2 - using substring - str.substr(0,num-3)
+function truncateString(str,num){
+    if(str.length === num) return str;
+    return str.slice(0,num-3)+"..."
+}
+
+console.log(truncateString("javascript",7))
