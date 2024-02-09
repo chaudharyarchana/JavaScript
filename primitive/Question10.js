576950a81de0a5ee3df66b6b52ef0ae7496b5466
Archana Kumari <archanakumari123.co@gmail.com>
2024-02-09 18:10:06 +0530

    "slice"

--- primitive/Question10.js
+++ primitive/Question10.js
@@ -4,8 +4,8 @@
 //Method 1 - using slice
 //Method 2 - using substring - str.substr(0,num-3)
 function truncateString(str,num){
-    if(str.length === num) return str;
+    if(str.length < num) return str;
     return str.slice(0,num-3)+"..."
 }
 
-console.log(truncateString("javascript",7))
+console.log(truncateString("js is fun",10))
