b55e117e84f270df8153f477e013611188f79d88
Archana Kumari <archanakumari123.co@gmail.com>
2024-02-12 11:54:43 +0530

    "validate indian phone number"

---
+++ primitive/Question12.js
@@ -0,0 +1,8 @@
+//  Create a regular expression to validate if the given input is valid Indian mobile number or not
+
+function validateMobile(str) {
+    const regexMobile = /^(\+91|0)?\s?\d{10}$/;
+    return regexMobile.test(str);
+  }
+
+console.log(validateMobile("+919352327705"))
